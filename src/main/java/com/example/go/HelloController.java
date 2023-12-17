d415eb9d4bf6b33a4c905e35b01ab8e43c980e3d
emo-mruczek <110098604+emo-mruczek@users.noreply.github.com>
2023-12-17 12:13:43 +0100

    fix: drawing a grid

--- src/main/java/com/example/go/HelloController.java
+++ src/main/java/com/example/go/HelloController.java
@@ -17,15 +17,21 @@ public class HelloController {
 
   int numRows = 3;
   int numColumns = 3;
+
+
   @FXML
   protected void onHelloButtonClick() {
     welcomeText.setText("Welcome to JavaFX Application!");
+
+    double cellWidth = grid.getWidth() / numColumns;
+    double cellHeight = grid.getHeight() / numRows;
+
     for (int row = 0; row < numRows; row++) {
       for (int col = 0; col < numColumns; col++) {
         Image image = new Image("C:/Users/krokc/Desktop/test.png");
         ImageView imageView = new ImageView(image);
-        imageView.setFitWidth(25);
-        imageView.setFitHeight(25);
+        imageView.setFitWidth(cellWidth);
+        imageView.setFitHeight(cellHeight);
         grid.add(imageView, row, col);
       }
     }
