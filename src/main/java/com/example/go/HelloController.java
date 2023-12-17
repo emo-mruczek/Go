e3aa1067c2a63bdc67df0f2e4ea03aa94343aa41
emo-mruczek <110098604+emo-mruczek@users.noreply.github.com>
2023-12-17 11:26:19 +0100

    feat: adding image to cells (very dirty)

--- src/main/java/com/example/go/HelloController.java
+++ src/main/java/com/example/go/HelloController.java
@@ -2,13 +2,32 @@ package com.example.go;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 
 public class HelloController {
+
   @FXML
   private Label welcomeText;
+  @FXML
+  private GridPane grid;
 
+  int numRows = 3;
+  int numColumns = 3;
   @FXML
   protected void onHelloButtonClick() {
     welcomeText.setText("Welcome to JavaFX Application!");
+    for (int row = 0; row < numRows; row++) {
+      for (int col = 0; col < numColumns; col++) {
+        Image image = new Image("C:/Users/krokc/Desktop/test.png");
+        ImageView imageView = new ImageView(image);
+        imageView.setFitWidth(25);
+        imageView.setFitHeight(25);
+        grid.add(imageView, row, col);
+      }
+    }
   }
 }
